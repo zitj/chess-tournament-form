e68591ffa4786fca819bdd9e0fb4ff5d8cbe5d42
Aleksandar Veljkovic <1eyedea2eyedeas@gmail.com>
2021-07-28 16:54:26 +0200

    Added validation messages

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="stylesheet" href="style/style.css" />
+		<script src="js/validation.js" defer></script>
+		<title>ПРИЈАВА ЗА ТУРНИР</title>
+	</head>
+	<body>
+		<form action="submit" method="">
+			<p>Пријава</p>
+			<div class="inputHolder">
+				<label for="name">Име и презиме</label>
+				<input type="text" name="name" placeholder="Унесите име и презиме" />
+				<span>Име и презиме морају бити одвојени размаком</span>
+			</div>
+			<div class="inputHolder">
+				<label for="phone">Број телефона</label>
+				<input type="text" name="phone" placeholder="+381 60 7555333" />
+				<span>Број мора почети са +381 и садржати минимум 8 бројева</span>
+			</div>
+			<div class="inputHolder">
+				<label for="email">E-mail адреса</label>
+				<input type="email" name="email" placeholder="primer@gmail.com" />
+				<span>Email мора садржати симбол @ нпр. korisnik@gmail.com</span>
+			</div>
+			<button type="submit">Пријави се</button>
+		</form>
+	</body>
+</html>
---
+++ js/validation.js
@@ -0,0 +1,34 @@
+console.log('We are connected!');
+
+const btn = document.querySelector('button');
+const inputs = document.querySelectorAll('input');
+const form = document.querySelector('form');
+let formValue = {};
+
+const patterns = {
+	name: /^([A-Za-z.!@?#"$%&:;()đšžćč *\+,\/;\-=[\\\]\^_{|}<>\u0400-\u04FF]){2,} ([A-Za-z.!@?#"$%&:;()đšžćč *\+,\/;\-=[\\\]\^_{|}<>\u0400-\u04FF]){3,}$/i,
+	phone: /^(\+381) ?(60|61|62|63|64|65|66|69|67|68|16) ?\d{6,}$/,
+	email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,9})?$/,
+};
+
+const validate = (field, regex) => {
+	if (regex.test(field.value)) {
+		field.className = 'valid';
+	} else {
+		field.className = 'invalid';
+	}
+};
+
+inputs.forEach((input) => {
+	input.addEventListener('keyup', (e) => {
+		setTimeout(() => {
+			validate(e.target, patterns[e.target.attributes.name.value]);
+		}, 300);
+		formValue[e.target.attributes.name.value] = e.target.value;
+	});
+});
+
+btn.addEventListener('click', (e) => {
+	e.preventDefault();
+	console.log(formValue);
+});
