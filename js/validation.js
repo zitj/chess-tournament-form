59618069ffe5efe54f1cfaa9a087f5da88a4fdb7
Aleksandar Veljkovic <1eyedea2eyedeas@gmail.com>
2021-07-29 13:04:45 +0200

    Added shake class on submit

--- js/validation.js
+++ js/validation.js
@@ -7,7 +7,7 @@ let formValue = {};
 let formValid = false;
 
 const patterns = {
-	name: /^([A-Za-z.!@?#"$%&:;()đšžćč *\+,\/;\-=[\\\]\^_{|}<>\u0400-\u04FF]){2,} ([A-Za-z.!@?#"$%&:;()đšžćč *\+,\/;\-=[\\\]\^_{|}<>\u0400-\u04FF]){3,}$/i,
+	name: /^([A-Za-z.!@?#"$%&:;()đšžćč *\+,\/;\-=[\\\]\^_{|}\u0400-\u04FF]){2,} ([A-Za-z.!@?#"$%&:;()đšžćč *\+,\/;\-=[\\\]\^_{|}<>\u0400-\u04FF]){3,}$/i,
 	phone: /^(\+381) ?(60|61|62|63|64|65|66|69|67|68|16) ?(\d{1,}) ?(\d{1,}) ?(\d{1,})?$/,
 	email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,9})?$/,
 };
@@ -42,17 +42,9 @@ const formValidityCheck = () => {
 
 inputs.forEach((input) => {
 	input.addEventListener('keyup', (e) => {
-		clearTimeout(typingTimer);
-		typingTimer = setTimeout(() => {
-			validate(e.target, patterns[e.target.attributes.name.value]);
-			formValidityCheck();
-		}, 300);
-
+		validate(e.target, patterns[e.target.attributes.name.value]);
+		formValidityCheck();
 		formValue[e.target.attributes.name.value] = e.target.value;
-	});
-
-	input.addEventListener('keydown', (e) => {
-		clearTimeout(typingTimer);
 	});
 });
 
@@ -60,5 +52,15 @@ btn.addEventListener('click', (e) => {
 	e.preventDefault();
 	if (formValid) {
 		form.submit();
+	} else {
+		inputs.forEach((input) => {
+			if (!input.classList.contains('valid')) input.classList.add('invalid');
+			if (input.classList.contains('invalid')) {
+				input.classList.add('shake');
+				input.addEventListener('animationend', () => {
+					input.classList.remove('shake');
+				});
+			}
+		});
 	}
 });
